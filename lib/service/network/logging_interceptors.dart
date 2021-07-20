f6e76c3f035cbfc3cd38229897c4281010a8afd2
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-20 15:10:45 +0800

    Add network layer and exceptions

---
+++ lib/service/network/logging_interceptors.dart
@@ -0,0 +1,52 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:cloudwalk/utility/utility.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+class LoggingInterceptors extends Interceptor {
+  int currentRequests = 0;
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (options.extra['customProgress'] == false) {
+      return super.onRequest(options, handler);
+    }
+
+    ++currentRequests;
+    EasyLoading.show(maskType: EasyLoadingMaskType.black);
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (response.extra['customProgress'] == false) {
+      return super.onResponse(response, handler);
+    }
+
+    --currentRequests;
+    if (0 >= currentRequests) {
+      EasyLoading.dismiss();
+    }
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    BotToast.showText(
+      text: NetworkException(err).getDetail(),
+      contentColor: Colors.red,
+    );
+
+    --currentRequests;
+    if (0 >= currentRequests) {
+      EasyLoading.dismiss();
+    }
+
+    if (err.response!.extra['customProgress'] == false) {
+      return super.onError(err, handler);
+    }
+
+    super.onError(err, handler);
+  }
+}
