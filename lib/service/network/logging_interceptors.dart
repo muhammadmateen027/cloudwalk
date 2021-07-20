f371f750253b035bae0bc430687543939b1c546e
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-20 19:30:55 +0800

    Add interceptors for network call

--- lib/service/network/logging_interceptors.dart
+++ lib/service/network/logging_interceptors.dart
@@ -1,11 +1,7 @@
-import 'package:bot_toast/bot_toast.dart';
-import 'package:cloudwalk/utility/utility.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class LoggingInterceptors extends Interceptor {
-  int currentRequests = 0;
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
@@ -13,7 +9,6 @@ class LoggingInterceptors extends Interceptor {
       return super.onRequest(options, handler);
     }
 
-    ++currentRequests;
     EasyLoading.show(maskType: EasyLoadingMaskType.black);
     super.onRequest(options, handler);
   }
@@ -24,24 +19,13 @@ class LoggingInterceptors extends Interceptor {
       return super.onResponse(response, handler);
     }
 
-    --currentRequests;
-    if (0 >= currentRequests) {
-      EasyLoading.dismiss();
-    }
+    EasyLoading.dismiss();
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    BotToast.showText(
-      text: NetworkException(err).getDetail(),
-      contentColor: Colors.red,
-    );
-
-    --currentRequests;
-    if (0 >= currentRequests) {
-      EasyLoading.dismiss();
-    }
+    EasyLoading.dismiss();
 
     if (err.response!.extra['customProgress'] == false) {
       return super.onError(err, handler);
